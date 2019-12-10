e03ad59d2f27c2b8ca4fe231e7756df8c0e48c8a
Kingsley Ramos <Kingsleyramos@gmail.com>
2019-12-09 18:46:19 -0800

    added routes to db.json and journal.json

--- public/assets/js/apiRoutes.js
+++ public/assets/js/apiRoutes.js
@@ -1,9 +1,14 @@
 const notesData = require("../../../db/db.json");
+const journalData = require("../../../journal.json");
 
 module.exports = function(app){
 
     app.get("/api/notes", function(req, res){
         res.json(notesData);
+    });
+
+    app.get("/api/journal", function(req, res){
+        res.json(journalData);
     });
 
 };
