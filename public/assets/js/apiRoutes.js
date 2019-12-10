3c34596ef057e10d090aa246133ebe378dc0f2a5
Kingsley Ramos <Kingsleyramos@gmail.com>
2019-12-09 18:38:33 -0800

    adde boilerplate and created first API Route

---
+++ public/assets/js/apiRoutes.js
@@ -0,0 +1,9 @@
+const notesData = require("../../../db/db.json");
+
+module.exports = function(app){
+
+    app.get("/api/notes", function(req, res){
+        res.json(notesData);
+    });
+
+};
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+require("./public/assets/js/apiRoutes")(app);
+require("./public/assets/js/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
