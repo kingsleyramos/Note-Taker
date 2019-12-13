ddff360862d010cabb879e7948afc1e7f333f034
Kingsley Ramos <Kingsleyramos@gmail.com>
2019-12-12 23:13:13 -0800

    added comments

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,11 +2,12 @@ const path = require("path");
 
 module.exports = function(app){
     
+    // Route to go to notes page
     app.get("/notes", function(req, res){
         res.sendFile(path.join(__dirname, "../public/notes.html"));
-        //console.log(path.join(__dirname, "../public/notes.html"))
     });
 
+    // Route to go to index page
     app.get("/", function(req, res){
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
